14335f7b92987d890a72d2c52265c5e76b881dd8
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-12-08 13:57:29 -0500

    added shuffle and draw (x) methods for Deck objects

--- object-oriented-programming/deck.py
+++ object-oriented-programming/deck.py
@@ -1,5 +1,6 @@
 
 import card
+import random
 
 class Deck:
     def __init__(self):
@@ -16,6 +17,17 @@ class Deck:
     def get_size(self):
         return self.size
 
+    def shuffle(self):
+        random.shuffle(self.contents)
+
+    def draw(self, number = 1):
+        hand = []
+        for x in range(number):
+            hand.append(card.Card.__str__(self.contents.pop()))
+        self.size -= number
+        print("There are now %d cards in the deck." % (self.size))
+        return hand
+
     def __str__(self):
         card_strings = []
         for c in self.contents:
@@ -24,4 +36,7 @@ class Deck:
 
 deck = Deck()
 print(deck.get_size())
-print(Deck.__str__(deck))
+deck.shuffle()
+print(deck.draw()) # draw 1
+print(deck.draw(3)) # draw 3
+# print(Deck.__str__(deck))
