d27b3395b652b4fbf77d78352e8dc2c3e8405b30
usman-tahir <tahir.usman.ali94@outlook.com>
2016-12-10 18:10:03 -0500

    Added a player name get method, and a hand shuffling method

--- object-oriented-programming/player.py
+++ object-oriented-programming/player.py
@@ -1,9 +1,14 @@
 
+import random
+
 class Player:
     def __init__(self, name):
         self.name = name
         self.hand = []
         self.score = 0
+    
+    def get_name(self):
+        return self.name
 
     def get_hand(self):
         return self.hand
@@ -16,7 +21,10 @@ class Player:
 
     def set_score(self, score):
         self.score = score
-
+    
+    def shuffle_hand(self):
+        random.shuffle(self.hand)
+        
     def show_hand(self):
         print("\n%s has the following cards:" % (self.name))
         for h in self.hand:
