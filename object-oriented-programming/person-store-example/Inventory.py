8b62a1ad98a442db930181473a5291fcf1ba45ec
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-12-15 10:29:11 -0500

    added a search method to the inventory, and added a 'stackable' and 'count' attribute to the Item class

--- object-oriented-programming/person-store-example/Inventory.py
+++ object-oriented-programming/person-store-example/Inventory.py
@@ -7,6 +7,14 @@ class Inventory:
         self.current_size = 0
         self.elements = []
 
+    def search(self, item_name):
+        found = False
+        for i in range(len(self.elements)):
+            if (self.elements[i].lower() == item_name.lower()):
+                found = True
+                break
+        return found
+
     def add(self, item):
         if (self.space == self.current_size):
             print("There is no more space to store this item.")
@@ -15,16 +23,19 @@ class Inventory:
             self.elements.append(item)
             print("A(n) %s has been added to the inventory." % (item.name))
 
-    def remove(self, name):
+    def remove(self, item_name):
+        found = Inventory.search(item_name)
         removed = False
-        for i in range(len(self.elements)):
-            if self.elements[i].name.lower() == name.lower():
-                self.elements.pop(i)
-                self.current_size -= 1
-                removed = True
-                break
+
+        if found:
+            for i in range(len(self.elements)):
+                if self.elements[i].name.lower() == item_name.lower():
+                    self.elements.pop(i)
+                    self.current_size -= 1
+                    removed = True
+                    break
         if removed:
             print("The %s has been removed from the inventory." \
-                % (name.lower()))
+                % (item_name.lower()))
         else:
-            print("The inventory does not contain a %s" % (name))
+            print("The inventory does not contain a %s" % (item_name.lower()))
--- object-oriented-programming/person-store-example/Item.py
+++ object-oriented-programming/person-store-example/Item.py
@@ -1,13 +1,21 @@
 
 class Item:
     _global_id = 0
-    def __init__(self, name, description, value = 0):
+    def __init__(self, name, description, value = 0, stackable = False):
         self.item_id = (Item._global_id + 1)
         Item._global_id += 1
         self.name = name
         self.value = value
         self.description = description
+        self.stackable = stackable
+        self.count = 1
+
+    def add_to_stack(self):
+        if (self.stackable == True):
+            self.count += 1
 
     def __str__(self):
-        return "\nID: %d\nName: %s\nValue: %d\nDescription: %s" \
-            % (self.item_id, self.name, self.value, self.description)
+        output = '''\nID: %d\nName: %s\nValue: %d\nDescription: %s
+Stackable: %s\nCount: %d''' % (self.item_id, self.name, \
+        self.value, self.description, str(self.stackable), self.count)
+        return output
