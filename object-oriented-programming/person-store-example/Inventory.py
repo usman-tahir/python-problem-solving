eac639b1dbc93034b6fefa1bd2dbe9ee2d402493
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-12-15 10:02:36 -0500

    added a remove method to the Inventory class (uses search by name)

--- object-oriented-programming/person-store-example/Inventory.py
+++ object-oriented-programming/person-store-example/Inventory.py
@@ -14,4 +14,17 @@ class Inventory:
             self.current_size += 1
             self.elements.append(item)
             print("A(n) %s has been added to the inventory." % (item.name))
-            print(str(self.elements[self.current_size - 1]))
+
+    def remove(self, name):
+        removed = False
+        for i in range(len(self.elements)):
+            if self.elements[i].name.lower() == name.lower():
+                self.elements.pop(i)
+                self.current_size -= 1
+                removed = True
+                break
+        if removed:
+            print("The %s has been removed from the inventory." \
+                % (name.lower()))
+        else:
+            print("The inventory does not contain a %s" % (name))
